23c1cfe169734c281998b67dd789f0eb96487a65
Izhan <57583461+1zhanN@users.noreply.github.com>
2024-04-19 02:40:10 +0500

    added navigation to the website
    added section scroll functionality using <Link> and <a>

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,34 +1,54 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   const [menu, setMenu] = useState("Home");
 
-
-
-
   return (
-    <div className='navbar'>
-        <img className='logo' src={assets.logo}/>
-        <ul className="navbar-menu">
-            <li onClick={()=>setMenu("Home")} className={menu=="Home"?"active":""}>Home</li>
-            <li onClick={()=>setMenu("Menu")} className={menu=="Menu"?"active":""}>Menu</li>
-            <li onClick={()=>setMenu("Mobile-App")} className={menu=="Mobile-App"?"active":""}>Mobile-App</li>
-            <li onClick={()=>setMenu("Contact-Us")} className={menu=="Contact-Us"?"active":""}>Contact-Us</li>
-        </ul>
-        <div className="navbar-right">
-            <img src={assets.search_icon} />
-            <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
-                <div className="dot"></div>
-            </div>
-
-            <button>Sign-In</button>
+    <div className="navbar">
+      <img className="logo" src={assets.logo} />
+      <ul className="navbar-menu">
+        <Link
+          onClick={() => setMenu("Home")}
+          className={menu == "Home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("Menu")}
+          className={menu == "Menu" ? "active" : ""}
+        >
+          Menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("Mobile-App")}
+          className={menu == "Mobile-App" ? "active" : ""}
+        >
+          Mobile-App
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("Contact-Us")}
+          className={menu == "Contact-Us" ? "active" : ""}
+        >
+          Contact-Us
+        </a>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
         </div>
-    </div>
-  )
-}
 
-export default Navbar
+        <button>Sign-In</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
